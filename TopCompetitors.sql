44f1cdfd892db76f317fe6d32928c45872346b68
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2023-02-19 16:49:07 +0545

    Top competitors files added

--- TopCompetitors.sql
+++ TopCompetitors.sql
@@ -1,3 +1,8 @@
-SELECT B.CONTINENT,FLOOR(AVG(A.POPULATION)) FROM CITY A JOIN 
-COUNTRY B ON A.CountryCode=B.Code
-GROUP BY B.Continent;
+SELECT H.hacker_id,H.name FROM Submissions S
+JOIN Challenges C ON S.challenge_id=C.challenge_id
+JOIN Difficulty D ON C.difficulty_level=D.difficulty_level
+JOIN Hackers H ON S.hacker_id=H.hacker_id AND 
+S.score=D.score GROUP BY 
+H.hacker_id,H.name
+HAVING COUNT(S.hacker_id) > 1
+ORDER BY COUNT(S.hacker_id) DESC,S.hacker_id ASC;
